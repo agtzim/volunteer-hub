d5d608bf5cc6572b57c0e1223580a4323f7b7e49
agtzim <agtzim@gmail.com>
2020-03-02 09:46:36 +0200

    initial commit

---
+++ src/main/resources/static/precache-manifest.e12702b432f70cfbf76e56225562ba1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe4d49f7c3e31076af89e5554733a0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e6a4ce246319537cb9f",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b76585a74832ce590e32",
+    "url": "/static/css/main.498ebc66.chunk.css"
+  },
+  {
+    "revision": "8e6a4ce246319537cb9f",
+    "url": "/static/js/2.abbb3c95.chunk.js"
+  },
+  {
+    "revision": "08627a56a3af4b8ed60c681cd0cd6d1a",
+    "url": "/static/js/2.abbb3c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76585a74832ce590e32",
+    "url": "/static/js/main.f121595c.chunk.js"
+  },
+  {
+    "revision": "8fd61fb962a6aedb5b26",
+    "url": "/static/js/runtime-main.82b1880e.js"
+  },
+  {
+    "revision": "9f7c7fa9e8619d801c75be61b7dddf0b",
+    "url": "/static/media/volunteer_1.9f7c7fa9.jpg"
+  },
+  {
+    "revision": "cfa2bb21adc4a8c1692f3818e8aa0797",
+    "url": "/static/media/volunteer_2.cfa2bb21.jpg"
+  },
+  {
+    "revision": "aef327208b59fefb869d9d6cbc277508",
+    "url": "/static/media/volunteer_3.aef32720.jpeg"
+  }
+]);
